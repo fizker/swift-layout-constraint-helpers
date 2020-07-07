5a631c920dc9a9f17d84979afdff353937087788
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-07-07 02:16:31 +0200

    Added some test code for ensuring that the public API exposes what needs to be exposed

--- Tests/LayoutConstraintHelpersTests/PublicAPITests.swift
+++ Tests/LayoutConstraintHelpersTests/PublicAPITests.swift
@@ -3,6 +3,31 @@ import LayoutConstraintHelpers
 
 final class PublicAPITests: XCTestCase {
 	func testExample() {
+		#if canImport(UIKit)
+		let view = UIView()
+		#elseif canImport(AppKit)
+		let view = NSView()
+		#endif
+
+		let size = CGSize(width: 1, height: 2)
+
+		_ = view.constraints(for: size)
+		_ = view.constraintForAspectRatio(size)
+		_ = view.constraintForAspectRatio(width: 1, height: 2)
+
+		_ = view.constraint(linking: .width, to: 5)
+		_ = view.constraint(linking: .width, to: 5, relation: .equal)
+		_ = view.constraint(linking: .width, to: 5, multiplier: 1)
+		_ = view.constraint(linking: .width, to: 5, relation: .equal, multiplier: 1)
+
+		_ = view.constraint(linking: .width, to: .height, of: view)
+		_ = view.constraint(linking: .width, to: .height, of: view, constant: 0)
+		_ = view.constraint(linking: .width, to: .height, of: view, multiplier: 1)
+		_ = view.constraint(linking: .width, to: .height, of: view, relation: .equal)
+		_ = view.constraint(linking: .width, to: .height, of: view, relation: .equal, constant: 0)
+		_ = view.constraint(linking: .width, to: .height, of: view, relation: .equal, multiplier: 1)
+		_ = view.constraint(linking: .width, to: .height, of: view, multiplier: 1, constant: 0)
+		_ = view.constraint(linking: .width, to: .height, of: view, relation: .equal, multiplier: 1, constant: 0)
 	}
 
 	static var allTests = [
